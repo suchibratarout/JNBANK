fedb278bb043ddc81e0516026a4a7568db278b7a
lucy <swainl1986@gmail.com>
2026-01-18 12:27:58 +0000

    first code

---
+++ tests/automationexcercize.spec.js
@@ -0,0 +1,18 @@
+import{test, expect} from '@playwright/test' 
+
+
+
+test ('login automation excercise', async ({page}) => {
+
+    let  url = "https://automationexercise.com/"
+    const emailid = ""
+    const Password =""
+
+
+});
+
+
+
+
+
+
---
+++ tests/login.spec.js
@@ -0,0 +1,31 @@
+import { test, expect } from "@playwright/test";
+
+
+test ('go to URL', async ({page}) => {
+
+await page.goto('https://portal.jnbank.co.uk/')
+
+// const title = await page.title();
+//   Print the title in console
+// console.log('Page Title is:', title);
+
+await expect(page).toHaveTitle('JN Bank UK | Savings & Personal Loans For UK Customers')
+
+//await page.click('text=Saving')
+//await page.click('href=/saving')
+// const loginBtn = page.getByRole('button', { name: 'Saving' });
+// await loginBtn.click();
+
+await page.getByRole('link', { name: 'Saving', exact: true }).click();
+
+await page.waitForTimeout(3000);
+
+await page.wa
+
+
+
+
+
+
+
+});
